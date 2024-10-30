2d386bbb2631843fd0be0c2b2090d88eb9c1d187
seonghoon-02 <tutle02@gmail.com>
2024-10-30 09:37:22 +0900

    no message

--- 241030_283.dart
+++ 241030_283.dart
@@ -1,6 +1,6 @@
 class Solution {
   void moveZeroes(List<int> nums) {
-        for(int i = nums.length - 1; i >=0 ; i--){
+    for(int i = nums.length - 1; i >=0 ; i--){
       if(nums[i] ==0){
         nums.removeAt(i);
         nums.add(0);
