d9565bb7bde64845ac8be652d263fb5959d1ac5d
seonghoon-02 <tutle02@gmail.com>
2024-11-25 09:52:02 +0900

    no message

---
+++ 241125_1232.dart
@@ -0,0 +1,14 @@
+bool checkStraightLine(List<List<int>> coordinates) {
+  int x0 = coordinates[0][0], y0 = coordinates[0][1];
+  int x1 = coordinates[1][0], y1 = coordinates[1][1];
+  int dx = x1 - x0;
+  int dy = y1 - y0;
+  
+  for (int i = 2; i < coordinates.length; i++) {
+    int x = coordinates[i][0], y = coordinates[i][1];
+    if ((x - x0) * dy != (y - y0) * dx) {
+      return false;
+    }
+  }
+  return true;
+}
